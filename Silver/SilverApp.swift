b6c712a7d8f55a7ddad822f676715aca80224c1b
Ajay Sharma <gridking111@gmail.com>
2026-01-19 12:46:34 +0200

    Initial Commit

---
+++ Silver/SilverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverApp.swift
+//  Silver
+//
+//  Created by Work Laptop on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SilverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SilverTests/SilverTests.swift
@@ -0,0 +1,17 @@
+//
+//  SilverTests.swift
+//  SilverTests
+//
+//  Created by Work Laptop on 19/01/2026.
+//
+
+import Testing
+@testable import Silver
+
+struct SilverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
